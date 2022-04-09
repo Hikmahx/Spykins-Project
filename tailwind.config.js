493adbead2fbd782e2b22c7c907e409ec1f46d04
Hikmahx <hikmayousuph@gmail.com>
2022-04-09 05:11:48 -1100

    app cleaned

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+    },
+    colors : {      
+      'light-gray': '#f2f2f2',
+      'very-light-gray': '#fafafa',
+      'grayish-blue': '#a3a5ae',
+      'very-dark-blue': '#4c4e61',
+      'transparent-white':'#ffffffbf', 
+      'transparent': 'transparent',
+      'white': '#ffffff',
+      'black': '#000000'
+    },
+  },
+  plugins: [],
+}
