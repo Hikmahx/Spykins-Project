dd1842684430e40596fecf2b1a85e21e1900e408
Hikmahx <hikmayousuph@gmail.com>
2022-04-09 05:22:13 -1100

    colors updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,13 @@ module.exports = {
   theme: {
     extend: {
     },
-    colors : {      
-      'light-gray': '#f2f2f2',
-      'very-light-gray': '#fafafa',
-      'grayish-blue': '#a3a5ae',
-      'very-dark-blue': '#4c4e61',
-      'transparent-white':'#ffffffbf', 
+    colors : {  
+      'soft-red': '#b91c1c',
+      'pale-red': '#fecaca',
+      'deep-red': '#7f1d1d',
+      'blue': '#1e3a8a',
+      'brown': '#78350f',
+      'light-grayish-blue': '#f3f4f6',
       'transparent': 'transparent',
       'white': '#ffffff',
       'black': '#000000'
